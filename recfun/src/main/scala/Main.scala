a6dea98c4e71b55fb0ec113541dac42320de0df4
Ararab <70100282+KDY05@users.noreply.github.com>
2025-09-06 03:37:31 +0900

    Exercise #1, #2

--- recfun/src/main/scala/Main.scala
+++ recfun/src/main/scala/Main.scala
@@ -1,15 +1,41 @@
+import scala.annotation.tailrec
+
 object Main {
 
   def main(args: Array[String]): Unit = {
-    println("Hello, World!")
+    println(pascal(0,2)) // 1
+    println(pascal(1,2)) // 2
+    println(pascal(1,3)) // 3
+
+    println(balance(":-)".toList)) // false
+    println(balance("())(".toList)) // false
+    println(balance("(())()()".toList)) // true
   }
 
   def pascal(c: Int, r: Int): Int = {
-    0
+    require(c >= 0 && r >= 0 && c <= r, "Invalid input")
+    if (c == 0 || c == r) {
+      1
+    } else {
+      pascal(c-1, r-1) + pascal(c, r-1)
+    }
   }
 
-  def balance(chars: List[Char]): Boolean= {
-    false
+  def balance(chars: List[Char]): Boolean = {
+    @tailrec
+    def balanceHelper(chars: List[Char], count: Int): Boolean = {
+      if (count < 0) false
+      else if (chars.isEmpty) count == 0
+      else if (chars.head == '(') {
+        balanceHelper(chars.tail, count + 1)
+      } else if (chars.head == ')') {
+        balanceHelper(chars.tail, count - 1)
+      } else {
+        balanceHelper(chars.tail, count)
+      }
+    }
+
+    balanceHelper(chars, 0)
   }
 
   def countChange(money: Int, coins: List[Int]): Int = {
