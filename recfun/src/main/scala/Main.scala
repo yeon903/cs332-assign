c70d4431f1607b35bd66b01f1aa7d275d46c3cf4
Ararab <70100282+KDY05@users.noreply.github.com>
2025-09-06 01:46:18 +0900

    initial commit

---
+++ recfun/src/main/scala/Main.scala
@@ -0,0 +1,19 @@
+object Main {
+
+  def main(args: Array[String]): Unit = {
+    println("Hello, World!")
+  }
+
+  def pascal(c: Int, r: Int): Int = {
+    0
+  }
+
+  def balance(chars: List[Char]): Boolean= {
+    false
+  }
+
+  def countChange(money: Int, coins: List[Int]): Int = {
+    0
+  }
+
+}
