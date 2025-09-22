e6ddacb92303648c68187757426707c296663f6c
Ararab <70100282+KDY05@users.noreply.github.com>
2025-09-22 20:22:19 +0900

    problem 4

--- objsets/src/main/scala/objsets/TweetSet.scala
+++ objsets/src/main/scala/objsets/TweetSet.scala
@@ -207,14 +207,24 @@ object GoogleVsApple {
   val google = List("android", "Android", "galaxy", "Galaxy", "nexus", "Nexus")
   val apple = List("ios", "iOS", "iphone", "iPhone", "ipad", "iPad")
 
-    lazy val googleTweets: TweetSet = ???
-  lazy val appleTweets: TweetSet = ???
+  lazy val googleTweets: TweetSet = {
+    TweetReader.allTweets.filter(tweet =>
+      google.exists(keyword => tweet.text.contains(keyword))
+    )
+  }
+  lazy val appleTweets: TweetSet = {
+    TweetReader.allTweets.filter(tweet =>
+      apple.exists(keyword => tweet.text.contains(keyword))
+    )
+  }
   
   /**
    * A list of all tweets mentioning a keyword from either apple or google,
    * sorted by the number of retweets.
    */
-     lazy val trending: TweetList = ???
+   lazy val trending: TweetList = {
+     googleTweets.union(appleTweets).descendingByRetweet
+   }
   }
 
 object Main extends App {
